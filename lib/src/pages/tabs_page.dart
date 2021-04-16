90ca52682a6b275a7926075facff0170c9294035
tomassasovsky <tomassasovskylinari@gmail.com>
2021-04-16 04:32:03 -0300

    Added Provider

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(color: Colors.red),
-          Container(
-            color: Colors.green,
-          )
-        ],
+    return ChangeNotifierProvider(
+      create: (_) => _NavigationModel(),
+      child: Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _NavigationBar(),
       ),
     );
   }
+}
+
+class _NavigationBar extends StatelessWidget {
+  const _NavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navigationModel.currentPage,
+      onTap: (value) => navigationModel.currentPage = (value),
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'For you'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Titles'),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  const _Pages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<_NavigationModel>(context);
+    return PageView(
+      controller: navigationModel.pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Container(color: Colors.red),
+        Container(
+          color: Colors.green,
+        )
+      ],
+    );
+  }
+}
+
+class _NavigationModel with ChangeNotifier {
+  int _currentPage = 0;
+  PageController _pageController = PageController();
+
+  int get currentPage => this._currentPage;
+  set currentPage(int value) {
+    this._currentPage = value;
+    _pageController.animateToPage(value, duration: Duration(milliseconds: 250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => this._pageController;
 }
